e409921c6c202b0aab90cd14e54e84fd3cc142cc
liulipack <liulipack@gmail.com>
2020-12-08 06:30:13 +0800

    clear

--- js/extra.js
+++
@@ -1,5 +0,0 @@
-// 内容链接加属性
-document.querySelectorAll('.md-content a').forEach((link) => {
-    link.setAttribute('target','_blank');
-    link.setAttribute('rel','nofollow noopener noreferrer');
-});
